4dddb1a951d4069fbd74213711b963feb8b5e6cf
ObviousNonsense <ObviousNonsense.1110@gmail.com>
2023-02-20 22:16:08 -0500

    Fix UI layout mistake

--- src/main.rs
+++ src/main.rs
@@ -1093,12 +1093,12 @@ fn setup_ui(ctx: &egui::Context, settings: &mut Settings, world: &mut World, fps
                 );
                 ui.label("");
                 ui.end_row();
-                ui.separator();
-                ui.separator();
-                ui.add(egui::Hyperlink::new(
-                    "https://github.com/ObviousNonsense/sand",
-                ));
             });
+            ui.separator();
+            ui.separator();
+            ui.add(egui::Hyperlink::new(
+                "https://github.com/ObviousNonsense/sand",
+            ));
         });
     // if settings.debug_mode {
     egui::Window::new("Debug Info")
